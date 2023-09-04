d5dbc0385ee357648acbc480a9b24b6436f0ddea
sunah <1004_sunah@naver.com>
2023-09-04 20:50:00 +0900

    firestore database 시작하기

--- album/script.js
+++ album/script.js
@@ -1,3 +1,28 @@
+// Firebase SDK 라이브러리 가져오기
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { getDocs } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+
+// Firebase 구성 정보 설정
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDtDmm6iJf4PIZtpJXfRRd8W-XKjuHxhgA",
+    authDomain: "sparta-1e178.firebaseapp.com",
+    projectId: "sparta-1e178",
+    storageBucket: "sparta-1e178.appspot.com",
+    messagingSenderId: "916403747858",
+    appId: "1:916403747858:web:0b8f305adc6a5709612ccb",
+    measurementId: "G-L8YQRNY77F"
+};
+
+
+// Firebase 인스턴스 초기화
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
 function openClose() {
     $('#postingBox').toggle();
 }
