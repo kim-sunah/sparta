854869a71bfa902c9b8616b8b74a8ca675344f70
sunah <1004_sunah@naver.com>
2023-09-07 23:19:09 +0900

    get

--- album/script.js
+++ album/script.js
@@ -3,7 +3,8 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { getDocs } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
 // Firebase 구성 정보 설정
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -32,8 +33,8 @@ $("#postingbtn").click(async function () {
     let doc = {
         'image': image,
         'title': title,
-        'content':content,
-        'date':date
+        'content': content,
+        'date': date
     }
 
     await addDoc(collection(db, "albums"), doc);
@@ -41,32 +42,36 @@ $("#postingbtn").click(async function () {
     window.location.reload();
 })
 
+
+
 $("#memory_save").click(async function () {
     $('#postingBox').toggle();
 })
-function makeCard() {
-    let image = $('#image').val();
-    let title = $('#title').val();
-    let content = $('#content').val();
-    let date = $('#date').val();
+// function makeCard() {
+//     let image = $('#image').val();
+//     let title = $('#title').val();
+//     let content = $('#content').val();
+//     let date = $('#date').val();
 
-    let temp_html = `
-    <div class="col">
-        <div class="card h-100">
-            <img src="${image}""
-                class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${title}</h5>
-                <p class="card-text">${content}</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-body-secondary">${date}</small>
-            </div>
-        </div>
-    </div>`;
-    $('#card').append(temp_html);
+//     let temp_html = `
+//     <div class="col">
+//         <div class="card h-100">
+//             <img src="${image}""
+//                 class="card-img-top" alt="...">
+//             <div class="card-body">
+//                 <h5 class="card-title">${title}</h5>
+//                 <p class="card-text">${content}</p>
+//             </div>
+//             <div class="card-footer">
+//                 <small class="text-body-secondary">${date}</small>
+//             </div>
+//         </div>
+//     </div>`;
+//     $('#card').append(temp_html);
 
-}
+// }
+
+
 
 let url = "http://spartacodingclub.shop/sparta_api/seoulair";
 fetch(url).then(res => res.json()).then(data => {
@@ -74,3 +79,32 @@ fetch(url).then(res => res.json()).then(data => {
     console.log(mise)
     $('#msg').text(mise);
 })
+
+let docs = await getDocs(collection(db, "albums"));
+docs.forEach((doc) => {
+    let row = doc.data();
+    console.log(row);
+
+    let image = row['image'];
+    let title = row['title'];
+    let content = row['content'];
+    let date = row['date'];
+    console.log("로그데이터 테스트 : " + row)
+
+    let temp_html = `
+<div class="col">
+<div class="card h-100">
+    <img src="${image}""
+        class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">${content}</p>
+    </div>
+    <div class="card-footer">
+        <small class="text-body-secondary">${date}</small>
+    </div>
+</div>
+</div>`;
+    $('#card').append(temp_html);
+});
+
