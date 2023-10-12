4281ef5a0512eff32e2663da53a39aab81dd3598
sunah <1004_sunah@naver.com>
2023-10-12 23:36:59 +0900

    조건문 중첩

--- JavaScript_total_class/week_1/13.js
+++ JavaScript_total_class/week_1/13.js
@@ -1,6 +1,16 @@
 //조건부 실행
 let x = 10;
 
+if(x>0){
+    console.log("x는 양수입니다")   //x는 양수입니다
+}
 
+//and조건(&&)
+(x>0) && console.log("x는 양수입니다")  //x는 양수입니다
 
-//(x>0) && console.log("x는 양수입니다")
+// or조건(||)
+//삼항 연산자와 단축키
+let y;
+let z=y||20;
+
+console.log(z)  //20
---
+++ JavaScript_total_class/week_1/14.js
@@ -0,0 +1,25 @@
+// falsy 한 값, truthy한 값
+
+if (0) {
+    console.log("Hello");
+}
+
+if ("") {
+    console.log("Hello");
+}
+
+if (null) {
+    console.log("Hello");
+}
+
+if (undefined) {
+   console.log("Hello");
+}
+
+if (NaN) {
+    console.log("Hello");
+}
+
+if (false) {
+    console.log("Hello");
+}
