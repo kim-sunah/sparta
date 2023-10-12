99a258df0424cab64929029802732e06649e4188
sunah <1004_sunah@naver.com>
2023-10-13 01:34:38 +0900

    남의풀이 추가

--- JavaScript_total_class/week_1/homework.js
+++ JavaScript_total_class/week_1/homework.js
@@ -38,3 +38,7 @@ function solution(absolutes, signs) {
     }
     return answer;
 }
+
+function solution(absolutes, signs) {
+    return absolutes.reduce((acc, val, i) => acc + (val * (signs[i] ? 1 : -1)), 0);
+}
