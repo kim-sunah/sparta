45ed201e2096912a59ef44f672add9ffd958e617
sunah <1004_sunah@naver.com>
2023-10-13 00:22:26 +0900

    배열 2

--- JavaScript_total_class/week_1/16.js
+++ JavaScript_total_class/week_1/16.js
@@ -35,10 +35,50 @@ console.log(fruits)     //[ '망고', '바나나', '오렌지' ]
 
 //3-5. splice
 //x 번째요소부터 y번째 요소까지 삭제 후 요소 추가
-fruits.splice(1,1,"포도")
+fruits.splice(1, 1, "포도")
 console.log(fruits) //[ '망고', '포도', '오렌지' ]
 
 //3-6. slice
 //x 번째요소부터 y번째 요소까지 새로운 배열로 만들어서 리턴
-let slickFruits = fruits.slice(1,2);
+let slickFruits = fruits.slice(1, 2);
 console.log(slickFruits)        //[ '포도' ]
+
+//forEach, map, filter, find
+let numbers = [1, 2, 3, 4, 5, 4, 6, 2, 4];
+
+//콜백 함수 : 매개변수 자리에 함수를 넣는것
+//아래의 경우 numbers에 있는 값들이 반복해서 순회
+numbers.forEach(function (item) {
+    console.log(item)
+    // 1
+    // 2
+    // 3
+    // 4
+    // 5
+    // 4
+    // 6
+    // 2
+    // 4
+});
+
+// (2) map
+//기존의 배열을 가공해서 새로운 배열을 리턴
+//항상 원본 배열의 길이만큼 리턴
+let newNumbers = numbers.map(function (item) {
+    return item * 2;
+})
+console.log(newNumbers)     //[ 2,  4, 6, 8, 10,8, 12, 4, 8]
+
+//(3) filter
+//조건에 해당되는 요소만 배열로 만들어서 리턴
+let filterdNumbers = numbers.filter(function(item){
+    return item ===5
+})
+console.log(filterdNumbers)     //[ 5 ]
+
+//(4) find
+//조건에 맞는 요소 왼쪽부터 순회해서  1개를 반환 
+let result = numbers.find(function(item){
+    return item >3
+})
+console.log(result)     //[ 5 ]
